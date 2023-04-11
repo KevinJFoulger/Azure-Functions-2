bf74300cd5ddadd6be897bcf8c7b79909d6922d8
Kevin Foulger <Kevin.Foulger@hotmail.co.uk>
2023-04-11 13:07:49 +0100

    main

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+<meta charset="utf-8" />
+
+<meta name="viewport" content="width-device, initial-scale=1.0">
+
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+<title>Azure Functions Test</title>
+
+<h1>Welcome to the Cloud</h1>
+
+<img src="img/cloud-small.jpg" />
+<p></p>
+<div class="text">
+The date and time is <?php echo date("l jS \of F Y h:i:s A");?>.
+</div>
+
+</body>
+</html>
